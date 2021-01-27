fd0eae401e8298843876ef96f907813b322f0864
jaeyoungjang2 <lion85488@gmail.com>
2021-01-26 23:48:58 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,26 +1,13 @@
 const title = document.querySelector("#title");
 
-const BASE_COLOR = "white";
-const OTHER_COLOR = "#7f8c8d";
+const CLICKED_CLASS = "clicked";
 
-function hadleClick(){
-    console.log(title.style.color);
-    const currentColor = title.style.color;
-    if (currentColor === BASE_COLOR){
-        title.style.color = OTHER_COLOR;
-        console.log(title.style.color);
-    }
-    else {
-        console.log(title.style.color);
-        title.style.color = BASE_COLOR;
-    }
+function handleClick(){
+    title.classList.toggle(CLICKED_CLASS);
 }
 
 function init(){
-    console.log("HI")
-    title.style.color = BASE_COLOR;
-    title.addEventListener("click", hadleClick);
+    title.addEventListener("click", handleClick);
 }
 
-console.log(title.style.color);
 init();
